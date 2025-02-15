640d72adf9f0560c1f80147c9b6b971df545df16
Jens Krogsbøll <jk@itealisten.dk>
2025-02-15 16:00:04 +0100

    Angular with tree

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {TreeComponent} from "./tree/tree.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [TreeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'frontend';
+}
